698b5d9714060695b7595b32905292e8cd727d68
Bayoura <Kristin.Seidelmann@gmx.de>
2021-12-08 14:43:12 +0100

    add window size condition

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
-const shareBtn_img = document.querySelectorAll('.share-btn');
+const shareBtn_div = document.querySelectorAll('.share-btn-container');
 const shareButtons_div = document.querySelector('.share-buttons');
 const socialInfo_div = document.querySelector('.social-information');
+const modal_div = document.querySelector('.modal');
 
-
-shareBtn_img.forEach(button => button.addEventListener('click', () => {
-    shareButtons_div.classList.toggle('active');
-    socialInfo_div.classList.toggle('hidden');
-})
+// window.addEventListener('resize', () => {
+//     shareButtons_div.classList.remove('active');
+//     socialInfo_div.classList.remove('hidden');
+// })
+        
+shareBtn_div.forEach(button => button.addEventListener('click', () => {           
+    if (window.innerWidth < 767) {               
+        shareButtons_div.classList.toggle('active');               
+        socialInfo_div.classList.toggle('hidden');          
+    } else {    
+        modal_div.classList.toggle('active');       
+    }       
+})    
 );
-
-// shareBtn_img.addEventListener('click', () => {
-//     shareButtons_div.classList.toggle('active');
-//     socialInfo_div.classList.toggle('hidden');
-// });
