9776dd54cc9ee108086065f4badec4032e0f9709
Bayoura <Kristin.Seidelmann@gmx.de>
2021-12-09 16:37:38 +0100

    add desktop pop-up and closing functionality

--- app.js
+++ app.js
@@ -1,19 +1,26 @@
 const shareBtn_div = document.querySelectorAll('.share-btn-container');
 const shareButtons_div = document.querySelector('.share-buttons');
 const socialInfo_div = document.querySelector('.social-information');
-const modal_div = document.querySelector('.modal');
+const popUp_div = document.querySelector('.pop-up');
 
-// window.addEventListener('resize', () => {
-//     shareButtons_div.classList.remove('active');
-//     socialInfo_div.classList.remove('hidden');
-// })
+window.addEventListener('resize', () => {
+    shareButtons_div.classList.remove('active');
+    socialInfo_div.classList.remove('hidden');
+    popUp_div.classList.remove('active');
+})
         
+window.addEventListener('keydown', e => {
+    if (e.key === 'Escape' && popUp_div.classList.contains('active')) {
+        popUp_div.classList.remove('active');
+    }
+})
+
 shareBtn_div.forEach(button => button.addEventListener('click', () => {           
     if (window.innerWidth < 767) {               
         shareButtons_div.classList.toggle('active');               
         socialInfo_div.classList.toggle('hidden');          
     } else {    
-        modal_div.classList.toggle('active');       
+        popUp_div.classList.toggle('active');       
     }       
 })    
 );
