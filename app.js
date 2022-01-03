6d8c6a16492a93678a4993fbd326eeb3abd619ee
Bayoura <Kristin.Seidelmann@gmx.de>
2022-01-03 17:18:33 +0100

    add overlay event listener

--- app.js
+++ app.js
@@ -2,16 +2,19 @@ const shareBtn_div = document.querySelectorAll('.share-btn-container');
 const shareIcons_div = document.querySelector('.share-icons');
 const socialInfo_div = document.querySelector('.social-information');
 const popUp_div = document.querySelector('.pop-up');
+const overlay_div = document.querySelector('.overlay');
 
 window.addEventListener('resize', () => {
     shareIcons_div.classList.remove('active');
     socialInfo_div.classList.remove('hidden');
     popUp_div.classList.remove('active');
+    overlay_div.classList.remove('active');
 })
         
 window.addEventListener('keydown', e => {
     if (e.key === 'Escape' && popUp_div.classList.contains('active')) {
         popUp_div.classList.remove('active');
+        overlay_div.classList.remove('active');
     }
 })
 
@@ -20,7 +23,13 @@ shareBtn_div.forEach(button => button.addEventListener('click', () => {
         shareIcons_div.classList.toggle('active');               
         socialInfo_div.classList.toggle('hidden');          
     } else {    
-        popUp_div.classList.toggle('active');       
+        popUp_div.classList.toggle('active');     
+        overlay_div.classList.toggle('active');  
     }       
 })    
 );
+
+overlay_div.addEventListener('click', () => {
+    popUp_div.classList.remove('active');
+    overlay_div.classList.remove('active');
+})
